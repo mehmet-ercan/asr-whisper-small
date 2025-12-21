884677915395d9e7bc319bc1ad50d2b7a686f5cf
Mehmet Ercan Akcan <mehmet.akcan@aurorabilisim.com>
2025-12-21 22:51:00 +0300

    yeni bir çeviri yapıldı.

--- cvrt_to_wav.py
+++ cvrt_to_wav.py
@@ -1,6 +1,6 @@
 import torchaudio
 
-waveform, sample_rate = torchaudio.load("giris.mp3")
+waveform, sample_rate = torchaudio.load("../../Downloads/se-hatıralar.m4a")
 
 # Gerekirse dönüştür (örneğin 16kHz'e)
 resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
